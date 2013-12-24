00d9c138680ab41ecfd5c7b052bd7df2fef0bf66
devsastr <devsastr@devsastr-System-Product-Name.(none)>
2013-12-24 20:02:44 +0400

    Try to tracking table test issue

--- tracking.py
+++ tracking.py
@@ -6,12 +6,12 @@ import segmentation.seqscanseg as segalg
 import segprops.segprops as segprops
 import sources.llserver as llsrv
 
-BACKGROUND_THRESHOLD = 160
+BACKGROUND_THRESHOLD = 170
 SEGMENT_THRESHOLD = 33
 SEGMENT_MINS = 100
-SEGMENT_MAXI = 190
+SEGMENT_MAXI = 170
 #SEGMENT_MINAVG = 165
-TRACKING_MINP = 0.97
+TRACKING_MINP = 0.98
 
 class TrackSegment:
 #    histo
@@ -31,6 +31,9 @@ class TrackSegment:
         self.histo = histo
         self.motion.append((cX / S, cY / S))
         self.isShown = True
+
+def toTrackSegment(segment):
+    return TrackSegment(segment.getHist(), segment.xS, segment.yS, segment.S)
 
 def filterSegment(segment):
     return (segment.S > SEGMENT_MINS and 
@@ -55,27 +58,27 @@ def findMaxPSegment(motionTable, seg):
         if (maxP < P):
             maxP = P
             maxPSeg = trkSeg
+    print maxP
     return (maxP, maxPSeg)
 
 
 def fillMotionTable(motionTable, segsProps):
     for trkSeg in motionTable:
         trkSeg.isShown = False
-
+    print "==========>"
     for k, v in segsProps.iteritems():
         (maxP, segment) = findMaxPSegment(motionTable, v)
         if (maxP < TRACKING_MINP):
             motionTable.append(TrackSegment(v.getHist(), v.xS, v.yS, v.S))
-        else:
+    	else:
             segment.add(v.getHist(), v.xS, v.yS, v.S)
-
-    for trkSeg in list(motionTable):
-        if (trkSeg.isShown == False):
-            trkSeg.timeOfEps += 1
-            if (trkSeg.timeOfEps > 10):
-                motionTable.remove(trkSeg)
-        else:
-            trkSeg.timeOfEps = 0
+#    for trkSeg in list(motionTable):
+#        if (trkSeg.isShown == False):
+#            trkSeg.timeOfEps += 1
+#            if (trkSeg.timeOfEps > 10):
+#                motionTable.remove(trkSeg)
+#        else:
+#            trkSeg.timeOfEps = 0
 
 def showMotion(motionTable, dst):
     for trkSeg in motionTable:
@@ -84,37 +87,55 @@ def showMotion(motionTable, dst):
                     cv2.line(dst, trkSeg.motion[i-1], trkSeg.motion[i],
                                     (155, 0, 0))
 
+def segmentate(img):
+    img_proc = cv2.medianBlur(img, 3)
+    (ret, img_proc) = cv2.threshold(img_proc, BACKGROUND_THRESHOLD, 255,cv2.THRESH_TOZERO)
+    return extractSegments(img_proc)
 
+def printSegments(srcSeg):
+    for key in srcSeg:
+        print  key, '=>', srcSeg[key].x0, srcSeg[key].y0, srcSeg[key].x1, srcSeg[key].y1
+
+def checkTracking():
+    src1 = cv2.imread("/home/devsastr/video/src/frm0.png")
+    src2 = cv2.imread("/home/devsastr/video/src/frm1.png")
+
+    src1 = cv2.cvtColor(src1, cv2.COLOR_BGR2GRAY, dstCn = 1)
+    src2 = cv2.cvtColor(src2, cv2.COLOR_BGR2GRAY, dstCn = 1)
+
+    seg1 = segmentate(src1)
+    seg2 = segmentate(src2)
+
+    motionTable = []
+    fillMotionTable(motionTable, seg1)
+    print ">===<"
+    fillMotionTable(motionTable, seg2)
 
 def main():
-#    cap = cv2.VideoCapture("../Astrohn_auto.mp4")
-    cap = llsrv.LLServerClient("192.168.1.9", 34123)
+#    cap = cv2.VideoCapture("../MJPG0007.AVI")
+    cap = llsrv.LLServerClient("192.168.1.13", 34123)
     cv2.namedWindow("Window")
     motionTable = []
     
     frm = 0
     while (cv2.waitKey(30) == -1):
         (ret, img) = cap.read()
-#	cv2.imwrite("../check/src_" + str(frm) + ".png", img)
-	if (len(img.shape) == 3):
-	    img_proc = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY, dstCn = 1)
-	else:
-	    img_proc = img
-	    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-	img = cv2.medianBlur(img, 3)
-        (ret, img_proc) = cv2.threshold(img_proc, BACKGROUND_THRESHOLD, 
-                                        255, cv2.THRESH_TOZERO)
-        segsProps = extractSegments(img_proc)
+#cv2.imwrite("/home/devsastr/video/src/frm" + str(frm) + ".png", img)
+        if (len(img.shape) == 3):
+            img_proc = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY, dstCn = 1)
+            segsProps = segmentate(img_proc)
+        else:
+            img_proc = img
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)    
+            segsProps = segmentate(img_proc)
         fillMotionTable(motionTable, segsProps)
         showSegments(segsProps, img)
         showMotion(motionTable, img)
+
+#        cv2.imwrite("/home/devsastr/video/dst/frm" + str(frm) + ".png", img)
         cv2.imshow("Window", img)
-#    	cv2.imwrite("../check/rslt_" + str(frm) + ".png", img)
-	frm += 1
-
-
-    return
-
+        frm += 1
 
 if (__name__ == '__main__'):
-    main()
+#    main()
+    checkTracking()
